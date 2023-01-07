7ed0c7641381595e8e28f7bda6e48083d0890a7a
kasbe shubham <shubhamkasbe4@gmail.com>
2023-01-07 18:10:35 +0530

    new code
    Signed-off-by: kasbe shubham <shubhamkasbe4@gmail.com>

---
+++ src/com/Corejava/PracticePrograms/DivisilbeBThreeSeven.java
@@ -0,0 +1,19 @@
+package com.Corejava.PracticePrograms;
+// Multiples of 3 and 7
+public class DivisilbeBThreeSeven {
+
+	public static void main(String[] args) 
+	{	
+		for(int i=0; i<=500; i++)
+		{
+			
+			if(i%3==0 && i%7==0)
+			{
+				System.out.println(i);
+			}
+		}
+		
+
+	}
+
+}
